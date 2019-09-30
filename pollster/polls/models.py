1f886a9f69f1d09ffb32d9ff0f12b7e440dd35d5
Arpik <arpiksargsyan@gmail.com>
2019-09-30 12:57:19 +0400

    Added str functions to show question and choice texts

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -7,8 +7,14 @@ class Question(models.Model):
     quesiton_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.quesiton_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)  n
+
+    def __str__(self):
+        return self.choice_text
