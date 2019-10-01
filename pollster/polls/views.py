ae99b34d55c98546c0d0fe995f00b55fc5f1a3e1
Arpik <arpiksargsyan@gmail.com>
2019-10-01 13:21:34 +0400

    Added try except block to check if there is no selected choice return detail.html page with error message. Else added 1 to votes.

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -26,5 +26,12 @@ def results(request, question_id):
 def vote(request, question_id):
     #print(request.POST['choice'])
     question = get_object_or_404(Question, pk = question_id)
-
-        
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #Redisplay the question votion form.
+        return render(request, 'polls/detail.html', {'question': question, 'error_message' : "You didn't select a choice." })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+            
