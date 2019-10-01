c9b12b5f679b6b41a1a799dde4881745c527c320
Arpik <arpiksargsyan@gmail.com>
2019-10-01 17:26:09 +0400

    Added  imports

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
@@ -21,7 +24,7 @@ def detail(request, question_id):
 #Get question and display results
 def results(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'polls/results.html', {'question' : question})
+    return render(request, 'polls/results.html' , {'question' : question})
 
 def vote(request, question_id):
     #print(request.POST['choice'])
@@ -29,10 +32,12 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk = request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        #Redisplay the question votion form.
-        return render(request, 'polls/detail.html', {'question': question, 'error_message' : "You didn't select a choice." })
+            #Redisplay the question votion form.
+            return render(request, 'polls/detail.html', {
+                'question': question, 
+                'error_message': "You didn't select a choice.", 
+                })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
