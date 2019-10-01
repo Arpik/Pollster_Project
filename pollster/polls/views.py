0c27fdc9129e8f5d2e17e9d2e46887b028f894e1
Arpik <arpiksargsyan@gmail.com>
2019-10-01 13:17:48 +0400

    Added vote view. Checks if there is an object with given id.

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -22,3 +22,9 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/results.html', {'question' : question})
+
+def vote(request, question_id):
+    #print(request.POST['choice'])
+    question = get_object_or_404(Question, pk = question_id)
+
+        
